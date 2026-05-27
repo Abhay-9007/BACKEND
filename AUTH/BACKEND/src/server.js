78ada6d8491eb9c36c69a233698129ebac62d367
Abhay Pratap Singh <abhaypratapsingh9007@gmail.com>
2026-05-27 22:13:28 +0530

    Initialize server and connect to database

---
+++ AUTH/BACKEND/src/server.js
@@ -0,0 +1,15 @@
+
+require('dotenv').config()
+
+const connectDB = require('./src/db/db.js')
+
+const app = require('./src/app.js')
+// const connectDB = require('./src/db/db.js')
+
+connectDB()
+
+app.listen(3000, () => {
+    console.log("Server is Running on port = 3000")
+})
+
+
