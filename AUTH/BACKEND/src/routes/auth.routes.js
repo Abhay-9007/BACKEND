9822ba196d7d67c0406224e7c0e96dfbcdc74663
Abhay Pratap Singh <abhaypratapsingh9007@gmail.com>
2026-05-28 22:07:40 +0530

    Create auth.routes.js

---
+++ AUTH/BACKEND/src/routes/auth.routes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const authController = require('../controllers/auth.controller')
+
+const router = express.Router()
+
+router.post('/register', authController.registerUser)
+
+
+// router.get('/test', (req, res) => {
+//     console.log("Cookies" , req.cookies)
+//     res.json({
+//         message : "test route",
+//         cookie : req.cookies
+//     })
+// })
+
+module.exports = router 
